c8a154fcfc1de6047419dfde94d80c94863a60e6
AlanVT <alanvitas27@gmail.com>
2022-07-30 12:01:11 -0300

    Ultimos cambios

---
+++ contacto.js
@@ -0,0 +1,6 @@
+function guardar(){
+    var nombre=document.getElementById("nombre").value;
+    var mail=document.getElementById("mail").value;
+    var tel=document.getElementById("tel").value;
+    var msg=document.getElementById("msg").value;
+}
