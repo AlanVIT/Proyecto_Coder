69d39e0600c079440f4e56e6508a8a89447ce7cc
AlanVT <alanvitas27@gmail.com>
2022-08-09 21:23:28 -0300

    form incluido

--- contacto.js
+++
@@ -1,6 +0,0 @@
-function guardar(){
-    var nombre=document.getElementById("nombre").value;
-    var mail=document.getElementById("mail").value;
-    var tel=document.getElementById("tel").value;
-    var msg=document.getElementById("msg").value;
-}
